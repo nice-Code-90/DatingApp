f4515bce9a6f4b3b06757bb024de96db974c9842
Milan Mazik <mazik1990@hotmail.com>
2025-08-13 23:35:49 +0200

    section 4: half way

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,15 +1,11 @@
 using API.Data;
 using API.Entities;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class MembersController(AppDbContext context) : ControllerBase
+    public class MembersController(AppDbContext context) : BaseApiController
     {
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
