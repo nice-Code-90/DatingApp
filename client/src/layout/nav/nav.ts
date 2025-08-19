c2e56a802c274ccc80f887296810292e351c6aa3
Milan Mazik <mazik1990@hotmail.com>
2025-08-19 22:06:08 +0200

    account service added + login method

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../../code/services/account-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './nav.html',
-  styleUrl: './nav.css'
+  styleUrl: './nav.css',
 })
 export class Nav {
+  private accountService = inject(AccountService);
+  protected creds: any = {};
 
+  login() {
+    this.accountService.login(this.creds).subscribe({
+      next: (result) => console.log(result),
+      error: (error) => alert(error.message),
+    });
+  }
 }
