0a948bc3197c7facf9531c675302e166d98299a4
Milan Mazik <mazik1990@hotmail.com>
2025-08-19 23:17:56 +0200

    Persisting the login with signals part2

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -9,15 +9,14 @@ import { AccountService } from '../../code/services/account-service';
   styleUrl: './nav.css',
 })
 export class Nav {
-  private accountService = inject(AccountService);
+  protected accountService = inject(AccountService);
   protected creds: any = {};
-  protected loggedIn = signal(false);
 
   login() {
     this.accountService.login(this.creds).subscribe({
       next: (result) => {
         console.log(result);
-        this.loggedIn.set(true);
+
         this.creds = {};
       },
       error: (error) => alert(error.message),
@@ -25,6 +24,6 @@ export class Nav {
   }
 
   logout() {
-    this.loggedIn.set(false);
+    this.accountService.logout();
   }
 }
