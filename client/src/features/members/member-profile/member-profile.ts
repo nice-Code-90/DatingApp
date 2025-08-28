973bf17f88acf81bb25b0ce63c2541164a68ba98
Milan Mazik <mazik1990@hotmail.com>
2025-08-28 21:27:25 +0200

    adding ther profile content

--- client/src/features/members/member-profile/member-profile.ts
+++ client/src/features/members/member-profile/member-profile.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from '../../../types/member';
 
 @Component({
   selector: 'app-member-profile',
   imports: [],
   templateUrl: './member-profile.html',
-  styleUrl: './member-profile.css'
+  styleUrl: './member-profile.css',
 })
 export class MemberProfile {
+  private route = inject(ActivatedRoute);
+  protected member = signal<Member | undefined>(undefined);
 
+  ngOnInit() {
+    this.route.parent?.data.subscribe((data) => {
+      this.member.set(data['member']);
+    });
+  }
 }
