4dd0207c519917716b1b3198e0d1a9e93d38d852
Milan Mazik <mazik1990@hotmail.com>
2025-08-30 17:24:13 +0200

    adding  an extension method for the ClaimsPrincipal

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,6 +1,7 @@
 using System.Security.Claims;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -36,10 +37,9 @@ namespace API.Controllers
         [HttpPut]
         public async Task<ActionResult> UpdateMember(MemberUpdateDto memberUpdateDto)
         {
-            var memberId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            if (memberId == null) return BadRequest("Oops - no id found in token");
+            var memberId = User.GetMemberId();
 
-            var member = await memberRepository.GetMemberByIdAsync(memberId);
+            var member = await memberRepository.GetMemberForUpdate(memberId);
             if (member == null) return BadRequest("Couldn't get member");
 
             member.DisplayName = memberUpdateDto.DisplayName ?? member.DisplayName;
@@ -47,7 +47,10 @@ namespace API.Controllers
             member.City = memberUpdateDto.City ?? member.City;
             member.Country = memberUpdateDto.Country ?? member.Country;
 
-            //memberRepository.Update(member);
+            member.User.DisplayName = memberUpdateDto.DisplayName ?? member.User.DisplayName;
+
+
+            memberRepository.Update(member);
             if (await memberRepository.SaveAllAsync()) return NoContent();
 
             return BadRequest("Failed to update member");
