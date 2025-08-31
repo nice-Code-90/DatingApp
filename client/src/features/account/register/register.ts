16c98aca7fce3b9ec5ef7708fe86ec4911bd579a
Milan Mazik <mazik1990@hotmail.com>
2025-08-31 22:04:31 +0200

    reactive forms added

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,11 +1,12 @@
-import { Component, inject, input, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RegisterCreds, User } from '../../../types/user';
+import { Component, inject, output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RegisterCreds } from '../../../types/user';
 import { AccountService } from '../../../core/services/account-service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
@@ -13,15 +14,30 @@ export class Register {
   private accountService = inject(AccountService);
   cancelRegister = output<boolean>();
   protected creds = {} as RegisterCreds;
+  protected registerForm: FormGroup = new FormGroup({});
+
+  ngOnInit() {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      email: new FormControl(),
+      displayName: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl(),
+    });
+  }
 
   register() {
-    this.accountService.register(this.creds).subscribe({
-      next: (response) => {
-        console.log(response);
-        this.cancel();
-      },
-      error: (error) => console.log(error),
-    });
+    console.log(this.registerForm.value);
+    // this.accountService.register(this.creds).subscribe({
+    //   next: (response) => {
+    //     console.log(response);
+    //     this.cancel();
+    //   },
+    //   error: (error) => console.log(error),
+    // });
   }
 
   cancel() {
