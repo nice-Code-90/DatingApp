7e2441e1baf2b9ad0069df6cb8435b4798c4d0d6
Milan Mazik <mazik1990@hotmail.com>
2025-09-13 00:27:59 +0200

    adding the like toggle

--- client/src/features/members/member-card/member-card.ts
+++ client/src/features/members/member-card/member-card.ts
@@ -15,4 +15,17 @@ export class MemberCard {
   member = input.required<Member>();
 
   protected hasLiked = computed(() => this.likeService.likeIds().includes(this.member().id));
+
+  toggleLike(event: Event) {
+    event.stopPropagation();
+    this.likeService.toggleLike(this.member().id).subscribe({
+      next: () => {
+        if (this.hasLiked()) {
+          this.likeService.likeIds.update((ids) => ids.filter((x) => x !== this.member().id));
+        } else {
+          this.likeService.likeIds.update((ids) => [...ids, this.member().id]);
+        }
+      },
+    });
+  }
 }
