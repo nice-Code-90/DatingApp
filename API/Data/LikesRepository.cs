7e8c5d85d73a443e111bf508dfc48408e0c97e22
Milan Mazik <mazik1990@hotmail.com>
2025-09-12 23:14:16 +0200

    implementing the repository methods

--- API/Data/LikesRepository.cs
+++ API/Data/LikesRepository.cs
@@ -1,38 +1,66 @@
 using System;
 using API.Entities;
 using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 
 namespace API.Data;
 
-public class LikesRepository : ILikesRepository
+public class LikesRepository(AppDbContext context) : ILikesRepository
 {
     public void AddLike(MemberLike like)
     {
-        throw new NotImplementedException();
+        context.Likes.Add(like);
     }
 
     public void DeleteLike(MemberLike like)
     {
-        throw new NotImplementedException();
+        context.Likes.Remove(like);
     }
 
-    public Task<IReadOnlyList<string>> GetCurrentMemberLikeIds(string memberId)
+    public async Task<IReadOnlyList<string>> GetCurrentMemberLikeIds(string memberId)
     {
-        throw new NotImplementedException();
+        return await context.Likes
+            .Where(x => x.SourceMemberId == memberId)
+            .Select(x => x.TargetMemberId)
+            .ToListAsync();
     }
 
-    public Task<MemberLike> GetMemberLike(string sourceMemberId, string targetMemberId)
+    public async Task<MemberLike?> GetMemberLike(string sourceMemberId, string targetMemberId)
     {
-        throw new NotImplementedException();
+        return await context.Likes.FindAsync(sourceMemberId, targetMemberId);
     }
 
-    public Task<IReadOnlyList<Member>> GetMemberLikes(string predicate, string memberId)
+    public async Task<IReadOnlyList<Member>> GetMemberLikes(string predicate, string memberId)
     {
-        throw new NotImplementedException();
+        var query = context.Likes.AsQueryable();
+
+        switch (predicate)
+        {
+            case "liked":
+                return await query
+                    .Where(x => x.SourceMemberId == memberId)
+                    .Select(x => x.TargetMember)
+                    .ToListAsync();
+
+            case "likedBy":
+                return await query
+                    .Where(x => x.TargetMemberId == memberId)
+                    .Select(x => x.SourceMember)
+                    .ToListAsync();
+            default:
+                var likeIds = await GetCurrentMemberLikeIds(memberId);
+
+                return await query
+                    .Where(x => x.TargetMemberId == memberId
+                        && likeIds.Contains(x.SourceMemberId))
+                    .Select(x => x.SourceMember)
+                    .ToListAsync();
+        }
     }
 
-    public Task<bool> SaveAllChanges()
+    public async Task<bool> SaveAllChanges()
     {
-        throw new NotImplementedException();
+        return await context.SaveChangesAsync() > 0;
     }
 }
--- API/Interfaces/ILikesRepository.cs
+++ API/Interfaces/ILikesRepository.cs
@@ -5,7 +5,7 @@ namespace API.Interfaces;
 
 public interface ILikesRepository
 {
-    Task<MemberLike> GetMemberLike(string sourceMemberId, string targetMemberId);
+    Task<MemberLike?> GetMemberLike(string sourceMemberId, string targetMemberId);
     Task<IReadOnlyList<Member>> GetMemberLikes(string predicate, string memberId);
     Task<IReadOnlyList<string>> GetCurrentMemberLikeIds(string memberId);
     void DeleteLike(MemberLike like);
