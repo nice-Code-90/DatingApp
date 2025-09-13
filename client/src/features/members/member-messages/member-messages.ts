6bc885bc1edcb73b42c1668f9868d957176d1c2c
Milan Mazik <mazik1990@hotmail.com>
2025-09-13 23:52:25 +0200

    getting the message thread in the angular app

--- client/src/features/members/member-messages/member-messages.ts
+++ client/src/features/members/member-messages/member-messages.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { MessageService } from '../../../core/services/message-service';
+import { MemberService } from '../../../core/services/member-service';
+import { Message } from '../../../types/message';
 
 @Component({
   selector: 'app-member-messages',
   imports: [],
   templateUrl: './member-messages.html',
-  styleUrl: './member-messages.css'
+  styleUrl: './member-messages.css',
 })
 export class MemberMessages {
+  private messageService = inject(MessageService);
+  private memberService = inject(MemberService);
 
+  protected messages = signal<Message[]>([]);
+
+  ngOnInit() {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    const memberId = this.memberService.member()?.id;
+    if (memberId) {
+      this.messageService.getMessageThread(memberId).subscribe({
+        next: (messages) => this.messages.set(messages),
+      });
+    }
+  }
 }
