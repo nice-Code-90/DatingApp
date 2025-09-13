c32f617faabcf19d7f5020fad019c715d196b2c8
Milan Mazik <mazik1990@hotmail.com>
2025-09-13 22:40:31 +0200

    settings up the angualr app for  messaging

--- client/src/features/messages/messages.ts
+++ client/src/features/messages/messages.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { MessageService } from '../../core/services/message-service';
+import { Message } from '../../types/message';
+import { PaginatedResult } from '../../types/pagination';
 
 @Component({
   selector: 'app-messages',
   imports: [],
   templateUrl: './messages.html',
-  styleUrl: './messages.css'
+  styleUrl: './messages.css',
 })
 export class Messages {
+  private messageService = inject(MessageService);
+  protected container = 'Inbox';
+  protected pageNumber = 1;
 
+  protected pageSize = 10;
+  protected paginatedMessages = signal<PaginatedResult<Message> | null>(null);
+
+  ngOnInit() {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.messageService.getMessages(this.container, this.pageNumber, this.pageSize).subscribe({
+      next: (response) => this.paginatedMessages.set(response),
+    });
+  }
 }
