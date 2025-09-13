71d123095459682b63d93190b2063b4be58327da
Milan Mazik <mazik1990@hotmail.com>
2025-09-13 20:06:56 +0200

    creating the message entity

--- API/Entities/Member.cs
+++ API/Entities/Member.cs
@@ -22,9 +22,15 @@ public class Member
     public List<Photo> Photos { get; set; } = [];
     [JsonIgnore]
     public List<MemberLike> LikedByMembers { get; set; } = [];
+
+    [JsonIgnore]
+    public List<Message> MessagesSent { get; set; } = [];
+
+    [JsonIgnore]
+    public List<Message> MessagesReceived { get; set; } = [];
+
     [JsonIgnore]
     public List<MemberLike> LikedMembers { get; set; } = [];
-
 
     [JsonIgnore]
     [ForeignKey(nameof(Id))]
