38f5c47535566720fd3ddc5df4d2d8ceec123dbe
Milan Mazik <mazik1990@hotmail.com>
2025-09-14 22:59:10 +0200

    updating the entities

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -24,8 +24,7 @@ public class AccountController(AppDbContext context, ITokenService tokenService)
         {
             DisplayName = registerDto.DisplayName,
             Email = registerDto.Email,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-            PasswordSalt = hmac.Key,
+            UserName = registerDto.Email,
             Member = new Member
             {
                 DisplayName = registerDto.DisplayName,
@@ -50,14 +49,6 @@ public class AccountController(AppDbContext context, ITokenService tokenService)
 
         if (user == null) return Unauthorized("Invalid email address");
 
-        using var hmac = new HMACSHA512(user.PasswordSalt);
-
-        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-        for (var i = 0; i < computedHash.Length; i++)
-        {
-            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
-        }
 
         return user.ToDto(tokenService);
 
@@ -65,6 +56,6 @@ public class AccountController(AppDbContext context, ITokenService tokenService)
 
     private async Task<bool> EmailExists(string email)
     {
-        return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
+        return await context.Users.AnyAsync(x => x.Email!.ToLower() == email.ToLower());
     }
 }
