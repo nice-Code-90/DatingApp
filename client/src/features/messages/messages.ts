269a1b87d11527811bb12062d393066ccd41567c
Milan Mazik <mazik1990@hotmail.com>
2025-09-14 20:57:53 +0200

    deleting messages- client

--- client/src/features/messages/messages.ts
+++ client/src/features/messages/messages.ts
@@ -39,6 +39,26 @@ export class Messages {
     });
   }
 
+  deleteMessage(event: Event, id: string) {
+    event.stopPropagation();
+    this.messageService.deleteMessage(id).subscribe({
+      next: () => {
+        const current = this.paginatedMessages();
+        if (current?.items) {
+          this.paginatedMessages.update((prev) => {
+            if (!prev) return null;
+            const newItems = prev.items.filter((x) => x.id !== id || []);
+
+            return {
+              items: newItems,
+              metadata: prev.metadata,
+            };
+          });
+        }
+      },
+    });
+  }
+
   get isInbox() {
     return this.fetchedContainer === 'Inbox';
   }
