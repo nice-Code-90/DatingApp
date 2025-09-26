5ca2f2d9143a90d50719b650b64ae6c2394f71e6
Milan Mazik <mazik1990@hotmail.com>
2025-09-27 00:35:41 +0200

    displaying online presence

--- client/src/core/services/presence-service.ts
+++ client/src/core/services/presence-service.ts
@@ -1,4 +1,4 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 
 import { ToastService } from './toast-service';
 import { HubConnection, HubConnectionBuilder, HubConnectionState } from '@microsoft/signalr';
@@ -12,6 +12,7 @@ export class PresenceService {
   private hubUrl = environment.hubUrl;
   private toast = inject(ToastService);
   hubConnection?: HubConnection;
+  onlineUsers = signal<string[]>([]);
 
   createHubConnection(user: User) {
     this.hubConnection = new HubConnectionBuilder()
@@ -20,12 +21,16 @@ export class PresenceService {
       .build();
 
     this.hubConnection.start().catch((error) => console.log(error));
-    this.hubConnection.on('UserOnline', (email) => {
-      this.toast.success(email + 'has connected');
+    this.hubConnection.on('UserOnline', (userId) => {
+      this.onlineUsers.update((users) => [...users, userId]);
     });
 
-    this.hubConnection.on('UserOffline', (email) => {
-      this.toast.info(email + 'has disconnected');
+    this.hubConnection.on('UserOffline', (userId) => {
+      this.onlineUsers.update((users) => users.filter((id) => id !== userId));
+    });
+
+    this.hubConnection.on('GetOnlineUsers', (userIds) => {
+      this.onlineUsers.set(userIds);
     });
   }
 
